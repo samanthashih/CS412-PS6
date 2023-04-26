0304b9e1488b1bafb7aa2dde6059c92c657358cc
Samantha Shih <sam.shih21@gmail.com>
2023-04-26 19:40:37 -0400

    finish component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_KEY } from "./api-keys";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div *ngIf="dataAvailable">
+      <h1>Weather Information for {{ cityName }}</h1>
+      <ul>
+        <li *ngFor="let weather of weatherData">
+          {{ weather.dt_txt }}: {{ weather.main.temp }}°C, {{ weather.weather[0].description }}
+        </li>
+      </ul>
+    </div>
+    <button (click)="fetchData()">Fetch Data</button>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PS6';
+
+export class AppComponent implements OnInit {
+  weatherData: any[] = [];
+  dataAvailable = false;
+  cityName: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {}
+
+  fetchData() {
+    this.http.get<any>(`https://api.openweathermap.org/data/2.5/forecast?q=${this.cityName}&appid=${API_KEY}&units=metric`)
+      .subscribe(response => {
+        this.weatherData = response.list;
+        this.dataAvailable = true;
+      });
+  }
 }
